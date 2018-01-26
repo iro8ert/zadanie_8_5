ce80ca6e7d9b5e0bd9e1e666a453e7cd4468521b
Robert Nowaczyk <iro8ert@o2.pl>
2018-01-26 14:21:12 +0100

    add changes

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
 	var a = 2;
 	var h = 5;
 	var triangleArea = a * h / 2;
-	console.log('Triangle field with base a: ' + a + ' and heigth h: ' + h + ' is equal to: ' + triangleArea);
+	window.alert('Triangle field with base a: ' + a + ' and heigth h: ' + h + ' is equal to: ' + triangleArea);

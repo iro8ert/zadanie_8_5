2e422bf2c89e1841c9432e474f21d731410503a8
Robert Nowaczyk <iro8ert@o2.pl>
2018-01-26 14:28:18 +0100

    add working script and index.html file

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
 	var a = 2;
 	var h = 5;
 	var triangleArea = a * h / 2;
-	window.alert('Triangle field with base a: ' + a + ' and heigth h: ' + h + ' is equal to: ' + triangleArea);
+	document.write('Triangle field with base a: ' + a + ' and heigth h: ' + h + ' is equal to: ' + triangleArea);
